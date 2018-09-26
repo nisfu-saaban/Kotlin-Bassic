95d9f585878bb4f9fe5e3a6d1d0810b12c7fe90a
nisfu-saaban <nisthy07@gmail.com>
2018-09-26 18:57:25 +0700

    Type Data

---
+++ TipeData.kt
@@ -0,0 +1,18 @@
+fun main(args: Array<String>) {
+
+    //val is like constant variable and its known as immutable in kotlin and can be initialized only single time.
+    val name: String = "name"
+    val age: Int = 19
+    val height: Double = 17.9
+    val widh: Float = 2.3f
+    val human: Boolean = true
+
+
+    //var is like general variable and its known as a mutable variable in kotlin and can be assigned multiple times.
+    var nama: String = "nama"
+    var umur: Int = 19
+    var tinggi: Double = 17.9
+    var lebar: Float = 2.3f
+    var manusia: Boolean = true
+
+}
